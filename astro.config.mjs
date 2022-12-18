ac6e1d7bee449a738e8a620e4e08bdf52348acd6
sujang.958 <enter7377@naver.com>
2022-12-18 19:05:06 +0900

    fix: fix not working on Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     config: {
       applyBaseStyles: true
     }
-  })]
+  })],
+  output: "server",
+  adapter: vercel()
 });
