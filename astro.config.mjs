fd4fb95452cc456537fd3dad9acc8af9c9555845
sujang958 <enter7377@naver.com>
2023-05-25 17:20:51 +0900

    ref: format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
-import svelte from "@astrojs/svelte";
+import svelte from "@astrojs/svelte"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: true
-    }
-  }), svelte()],
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: true,
+      },
+    }),
+    svelte(),
+  ],
   output: "server",
-  adapter: vercel()
-});
+  adapter: vercel(),
+})
