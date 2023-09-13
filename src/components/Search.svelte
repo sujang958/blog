77cb8e8f310d985ce53300c2e018b811a22ff518
sujang958 <sujangdev@gmail.com>
2023-09-13 22:00:23 +0900

    perf: migrate to bun

---
+++ src/components/Search.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { getCollection, type CollectionEntry } from "astro:content"
+
+  export let posts: CollectionEntry<"post">[] = <any>[]
+
+  getCollection("post").then((collection) => (posts = collection))
+</script>
+
+
+<button type="button">
+  asdf
+</button>
