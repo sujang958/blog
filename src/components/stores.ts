6240ad7210e1a13974ab247c4ac9e1dca2485181
sujang958 <sujangdev@gmail.com>
2024-07-02 18:21:34 +0900

    feat: return of svelte

--- src/components/stores.ts
+++ src/components/stores.ts
@@ -1,3 +1,6 @@
+import { persisted } from "svelte-persisted-store"
 import { writable } from "svelte/store"
 
 export const searchWindowShown = writable(false)
+
+export const dark = persisted("dark", false)
